9783cf4b8aa312aebc026124417968559d65de58
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-14 12:34:41 +0100

    created function to generate 10 array with random number inside

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-/*
+/* snack1
 Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
- Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
+const numbers = [];
+
+let sum = 0;
+while (sum < 50) {
+    const newNumber = parseInt(prompt("Inserisci un numero casuale"));
+    if(!isNaN(newNumber)){
+        sum += newNumber;
+        numbers.push(newNumber);
+    }
+}
+
+console.log('numbers:', numbers);
+console.log('Total sum: ', sum);
 */
 
-let array = [];
+/* Snack3
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+    Ogni volta che ne crei uno, stampalo.
+*/
 
-let userinput = parseInt(prompt('Inserisci un numero casuale')); 
+
+const userNumber = parseInt(prompt('Inserisci un numero'));
+
+for (let index = 0; index < userNumber; index++) {
+    console.log(randomNumberArray());
+}
+
+function randomNumberArray(){
+    const newArray = [];
+    for (let index = 0; index < 10; index++) {
+        newArray.push(getRndInteger(1,100));
+    }
+    return newArray;
+}
